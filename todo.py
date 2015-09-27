5e52737ccb550f2c546ec7137cf3e141d9edcd67
Olav Vatne <olavvatne@gmail.com>
2015-09-27 16:53:05 +0200

    Todo list

---
+++ todo.py
@@ -0,0 +1,6 @@
+__author__ = 'olav'
+
+#TODO: list
+# Opener for current dataset. See mapDataset, filesizes gets to big with pikl
+# Recreate network in paper. 3 layer.
+# Naive no error correction - test
