125624fdee794259df39ee5376b6e2031cc968e0
Olav Vatne <olavvatne@gmail.com>
2016-01-30 14:59:24 +0100

    Files and todos for precision and recall curves.

---
+++ tools/measurement/precisionrecall.py
@@ -0,0 +1,10 @@
+__author__ = 'olav'
+
+'''
+TODO: Create all possible patches in test dataset.
+TODO: Create predictions using system for all these patches.
+TODO: Vary threshold and collect precision and recall for each patch.
+TODO: Implement within x pixel , done by Hinton and Minh.
+TODO: Upload to database, and display using rickshaw. (Should be done)
+TODO: Do this test at the end of a training session, so PR curves for all experiments automatically (optional)
+'''
