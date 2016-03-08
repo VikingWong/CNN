d49ec0270d2244d6065279a27e217f1c8b646821
Olav Vatne <olavvatne@gmail.com>
2016-03-08 14:43:19 +0100

    Removes logistic regression layer Not used by system at all

--- elements/__init__.py
+++ elements/__init__.py
@@ -1,4 +1,3 @@
-from elements.logisticregression import LogisticRegression
 from elements.hidden import HiddenLayer
 from elements.convpool import ConvPoolLayer
 from elements.custom import OutputLayer
